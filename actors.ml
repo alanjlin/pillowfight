3d3b13df562a169f0f4a9432d76efd7357894a7d
alanjlin <blast408@gmail.com>
2018-04-28 22:59:13 -0400

    updated actors

--- actors.ml
+++ actors.ml
@@ -1,13 +1,3 @@
-type girl = Bloom | Soap | Margarinecup
-
-type people = Professor| Girl
-
-type furniture = Bed | Walls
-
-type pillow = Regular | Hard | Repeating
-
-type obj = People | Furniture | Pillow
-
 type info = {
   max_speed: int;
   throw_power: int;
@@ -17,3 +7,13 @@ type info = {
   hitbox: (int * int) list;
   layer: int;
 }
+
+type girl = Bloom of info | Soap of info | Margarinecup of info
+
+type people = Professor| Girl
+
+type furniture = Bed | Walls
+
+type pillow = Regular | Hard | Repeating
+
+type obj = People | Furniture | Pillow
