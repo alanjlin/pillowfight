8e022b274f81313a36fd8df3fb4359cf3e0c4e52
alanjlin <blast408@gmail.com>
2018-04-28 23:53:19 -0400

    added makefile

---
+++ Makefile
@@ -0,0 +1,9 @@
+
+build:
+	ocamlbuild -use-ocamlfind \
+		-plugin-tag "package(js_of_ocaml.ocamlbuild)" \
+		-no-links \
+		main.d.js
+
+clean:
+	ocamlbuild -clean
--- state.ml
+++ state.ml
@@ -1,6 +1,13 @@
 open Actors
 
-type move = Space | Left | Right | Up | Down
+
+type move = {
+  mutable up: bool;
+  mutable down: bool;
+  mutable left: bool;
+  mutable right: bool;
+  mutable space: bool;
+}
 
 type collision =
   | GirlOnGirl
@@ -18,6 +25,14 @@ type st = {
   collisions: collision list;
   scores: (girl * int) list;
   time: float;
+}
+
+let pressed_keys = {
+  up = false;
+  down = false;
+  left = false;
+  right = false;
+  space = false;
 }
 
 let girls (s: st) = s.girls
@@ -39,3 +54,24 @@ let move_handler m s = failwith "unimplemented"
 let collisionHandler c s = failwith "unimplemented"
 
 let isColliding o1 o2 = failwith "unimplemented"
+
+(* Keydown event handler translates a key press *)
+let keydown evt =
+  let () = match evt##keyCode with
+  | 38 | 32 | 87 -> pressed_keys.up <- true
+  | 39 | 68 -> pressed_keys.right <- true
+  | 37 | 65 -> pressed_keys.left <- true
+  | 40 | 83 -> pressed_keys.down <- true
+  | 66 -> pressed_keys.bbox <- (pressed_keys.bbox + 1) mod 2
+  | _ -> ()
+  in Js._true
+
+(* Keyup event handler translates a key release *)
+let keyup evt =
+  let () = match evt##keyCode with
+  | 38 | 32 | 87 -> pressed_keys.up <- false
+  | 39 | 68 -> pressed_keys.right <- false
+  | 37 | 65 -> pressed_keys.left <- false
+  | 40 | 83 -> pressed_keys.down <- false
+  | _ -> ()
+  in Js._true
