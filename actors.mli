8664e78d796202b58529f7ff5ce40cb6c4b43621
Ryan Tang <ryantang@dhcp-clara-3340.eduroam.cornell.edu>
2018-04-30 23:12:13 -0400

    fixed types of actors hopefully

--- actors.mli
+++ actors.mli
@@ -1,20 +1,24 @@
-(* record storing all the information of the variants *)
-type info = {
-  move_speed: int; (*only applies to girls, when player moving*)
-  fly_speed: int; (*speed of projectiles*)
-  throw_power: int; (**)
-  recovery_time: int;
-  direction: int; (*no direction = 0; up = 1; right = 2; down = 3; left = 4*)
-  coordinate: int * int;
-  hitbox: (int * int) list;
-  has_pillow: bool;
-}
+module type Actors = sig
 
-(* variant representing the types of girl *)
-type girl = Bloom | Soap | Margarinecup
+  (* record storing all the information of the variants *)
+  type info = {
+    move_speed: int; (*only applies to girls, when player moving*)
+    fly_speed: int; (*speed of projectiles*)
+    throw_power: int; (**)
+    recovery_time: int;
+    direction: int; (*no direction = 0; up = 1; right = 2; down = 3; left = 4*)
+    coordinate: int * int;
+    hitbox: (int * int) list;
+    has_pillow: bool;
+  }
 
-(* variant representing the types of furniture *)
-type furniture = Walls
+  (* variant representing the types of girl *)
+  type girl = Bloom | Soap | Margarinecup
 
-(* variant representing the types of pillow *)
-type pillow = Regular
+  (* variant representing the types of furniture *)
+  type furniture = Walls
+
+  (* variant representing the types of pillow *)
+  type pillow = Regular
+
+end
