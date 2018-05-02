cc258177cfb40ce45b141f3b531109a84f39bbbe
vagrant <vagrant@vm3110-2018sp>
2018-05-01 20:02:04 -0400

    only committing bc i have to

--- display.ml
+++ display.ml
@@ -3,27 +3,21 @@ module Html = Dom_html
 let js = Js.string
 let document = Html.document
 
-let dims = (400, 400) (* width, height *)
+(* [update_draw_actor context] draws the sprite image at its updated coordinates.
+   currently hardcoded for just updating the sprite *)
+let update_draw_actor actor context =
+  let img = (Dom_html.createImg Dom_html.document) in
+  img##src <- (Js.string "./pics/sprite.png");
+  let dx = fst actor.coordinate in
+  let dy = snd actor.coordinate in
+  context##drawImage_full(img, 0, 0, 20, 20, dx, dy, 20, 20)
 
-(* [context_of_canvas canvas] returns the context of [canvas] *)
-let context_of_canvas canvas = canvas##getContext (Dom_html._2d_)
-
-(* let render sprite (posx,posy) =
-  let context = sprite.context in
-  let (sx, sy) = sprite.params.src_offset in
-  let (sw, sh) = sprite.params.frame_size in
-  let (dx, dy) = (posx,posy) in
-  let (dw, dh) = sprite.params.frame_size in
-  let sx = sx +. (float_of_int !(sprite.frame)) *. sw in
-  (*print_endline (string_of_int !(sprite.frame));*)
-  (*context##clearRect(0.,0.,sw, sh);*)
-   context##drawImage_full(sprite.img, sx, sy, sw, sh, dx, dy, dw, dh) *)
-
-(* [draw_actor context] draws the sprite image *)
+(* [draw_actor context] draws the sprite image. currently hardcoded for just
+the sprite *)
 let draw_actor context =
   let img = (Dom_html.createImg Dom_html.document) in
-  img##src <- (Js.string "./pics/sprite.png")
-  context##drawImage_full(img, 0, 0, 20, 20, 200, 200, 20, 20)
+  img##src <- (Js.string "./pics/sprite.png");
+  context##drawImage_full(img, 0, 0, 20, 20, 200, 200, 20, 20);
 
 (* [draw_bg context] draws the background *)
 let draw_bg context =
@@ -36,7 +30,7 @@ let draw_bg context =
   *)
   context##drawImage_full(img, 0, 0, 400, 400, 0, 0, 400, 400)
 
-(* [draw_state context state] currently hard-coded to work with  *)
+(* [draw_state context state] currently hard-coded to actor and bg *)
 let draw_state context state =
   draw_actor context
   draw_bg context
