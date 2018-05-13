ead4a4a2b141aae946cbe2ab9edfd3f6021f8257
vagrant <vagrant@vm3110-2018sp>
2018-05-13 18:53:52 -0400

    Wrote specs for functions in display

--- display.ml
+++ display.ml
@@ -6,10 +6,15 @@ module Html = Dom_html
 let js = Js.string
 let document = Html.document
 
-(* [update_girl_imgsrc info context] assigns the proper img_src based on
+(* effects: [update_girl_imgsrc info context] assigns the proper img_src based on
    if the girl represented by [info] has a pillow or not. If the girl does,
    then the image will be set to the throwing sprite, otherwise it will be
-set to the normal sprite.*)
+   set to the normal sprite.
+   returns: nothing
+   raises: nothing
+   requires:
+    - context : Dom_html.canvasRenderingContext2D Js.t
+    - girl : Actors.girl *)
 let update_girl_imgsrc info context = match info with
   | Bloom b -> if b.has_pillow then b.img_src <- _BTHROWSPRITE
                else b.img_src <- _BNORMALSPRITE
@@ -18,26 +23,30 @@ let update_girl_imgsrc info context = match info with
   | Margarinecup m -> if m.has_pillow then m.img_src <- _MTHROWSPRITE
                       else m.img_src <- _MNORMALSPRITE
 
-(* [update_draw_actor context] draws the sprite image at its updated coordinates. *)
-let update_draw_actor info context =
-  let _ = update_girl_imgsrc info context in
+(* effects: [update_draw_actor context] draws the sprite image at its coordinates
+  returns: nothing
+  raises: nothing
+  requires:
+   - context : Dom_html.canvasRenderingContext2D Js.t
+   - girl : Actors.girl
+*)
+let update_draw_actor girl context =
+  let _ = update_girl_imgsrc girl context in
   let img = (Dom_html.createImg Dom_html.document) in
   (* added the line below to type check *)
-  let info' = match info with Bloom b -> b | Soap so -> so | Margarinecup m -> m in
-  img##src <- (Js.string info'.img_src);
+  let girl' = match girl with Bloom b -> b | Soap so -> so | Margarinecup m -> m in
+  img##src <- (Js.string girl'.img_src);
   context##drawImage_full(img, 0., 0., _GIRLSIZE, _GIRLSIZE,
-                          float_of_int (fst info'.coordinate),
-                          float_of_int (snd info'.coordinate),
+                          float_of_int (fst girl'.coordinate),
+                          float_of_int (snd girl'.coordinate),
                           _GIRLSIZE, _GIRLSIZE)
 
-(* [draw_actor context] draws the sprite image. currently hardcoded for just
-the sprite *)
-(* let draw_actor context =
-  let img = (Dom_html.createImg Dom_html.document) in
-  img##src <- (Js.string "./pics/sprite.png");
-  context##drawImage_full(img, 0., 0., 20., 20., 200., 200., 20., 20.) *)
-
-(* [draw_pillow st] loops through [st.pillows] to draw each pillow *)
+(* effects: [draw_pillow st] loops through [st.pillows] to draw each pillow on
+   [context]
+   returns: nothing
+   raises: nothing
+   requires:
+   - context : Dom_html.canvasRenderingContext2D Js.t *)
 let rec draw_pillow (context: Dom_html.canvasRenderingContext2D Js.t) st =
   match st.pillows with
   | [] -> ()
@@ -51,27 +60,38 @@ let rec draw_pillow (context: Dom_html.canvasRenderingContext2D Js.t) st =
                             _PILLOWSIZE, _PILLOWSIZE);
     draw_pillow context {st with pillows = t}
 
-(* [draw_bg context] draws the background onto [context].
-   static, never does anything else *)
+(* effects: [draw_bg context] draws our background file onto [context].
+   returns: nothing
+   raises: nothing
+   requires:
+   - context : Dom_html.canvasRenderingContext2D Js.t *)
 let draw_bg context =
   let img = (Dom_html.createImg Dom_html.document) in
   img##src <- (Js.string "./pics/background.png");
-  (* ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
-     sx = 0, sy = 0, sWidth = 400, sHeight = 400,
-     dx = 0, dy = 0, dWidth = 400, dHeight = 400,
-     bg name = background.png
-  *)
   context##drawImage_full(img, 0., 0., _BGSIZE, _BGSIZE,
                           0., 0., _BGSIZE, _BGSIZE)
 
-(* [draw_scoreboard context] draws the scoreboard onto [context].
-   static, never does anything else *)
+(* effects: [draw_scoreboard context] draws the scoreboard onto [context].
+   returns: nothing
+   raises: nothing
+   requires:
+   - context : Dom_html.canvasRenderingContext2D Js.t
+   - "./pics/scoreboard.png" is a file that exists *)
 let draw_scoreboard context =
   let img = (Dom_html.createImg Dom_html.document) in
   img##src <- (Js.string "./pics/scoreboard.png");
   context##drawImage_full(img, 0., 0., _SBWIDTH, _SBHEIGHT,
                           _BGSIZE, 0., _SBWIDTH,  _SBHEIGHT)
 
+(* effects: [draw_score context score name] draws [score] of the girl
+   represented by [name] onto [context].
+   returns: nothing
+   raises: nothing
+   requires:
+   - context : Dom_html.canvasRenderingContext2D Js.t
+   - score : int
+   - name : string
+*)
 let draw_score context score name =
   let score_coord =
     if name = "bloom" then (_BSCORECOORDX, _BSCORECOORDY)
@@ -82,27 +102,43 @@ let draw_score context score name =
   context##font <- (Js.string "50px 'Magical'");
   context##fillText (Js.string (string_of_int score), fst score_coord, snd score_coord)
 
-(* [draw_time context time] draws [time] (given by state.time) in the time box
-on the scoreboard *)
+(* effects: [draw_time context time] draws [time] (given by state.time)
+   in the time box on the scoreboard.
+   returns: nothing
+   raises: nothing
+   requires:
+   - context : Dom_html.canvasRenderingContext2D Js.t
+   - time : int *)
 let draw_time context time =
   context##fillStyle <- (Js.string "white");
   context##font <- (Js.string "25px 'Magical'");
   context##fillText (Js.string (string_of_int time), _TIMECOORDX, _TIMECOORDY)
 
-(* [wipe context] resets the context to a blank square context with
-   side length [_BGSIZE] *)
-let wipe (context: Dom_html.canvasRenderingContext2D Js.t) =
-  context##clearRect (0., 0., _BGSIZE, _BGSIZE)
+(* effects: [wipe context] resets the context to a blank square context with
+   side length [_BGSIZE].
+   returns: nothing
+   raises: nothing
+   requires:
+   - context: Dom_html.canvasRenderingContext2D Js.t *)
+  let wipe context =
+    context##clearRect (0., 0., _BGSIZE, _BGSIZE)
 
-(* [draw_state context state] draws the following items based on info from
-   [state]:
+(* effects: [draw_state context state] draws the following items based on info
+   from [state]:
    - background
    - score for each girl
    - time
    - pillows
    - each girl
+   returns: nothing
+   raises: Failure "not possible" if [state.bloom, state.soap, state.mcup]
+   somehow, for some god forsaken reason, doesn't have type
+   [Bloom * Soap * Margarinecup]
+   requires:
+   - context: Dom_html.canvasRenderingContext2D Js.t
+   - state: State.st
 *)
-let draw_state (context: Dom_html.canvasRenderingContext2D Js.t) state =
+let draw_state context state =
   wipe context;
   match state.bloom, state.soap, state.mcup with
   | Bloom b, Soap so, Margarinecup m ->
