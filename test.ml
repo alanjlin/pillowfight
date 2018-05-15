0f0f9192e91d3b1355031b4ca0ee9869966c8019
Ryan Tang <ryantang@dhcp-rhodes-362.eduroam.cornell.edu>
2018-05-15 16:14:13 -0400

    final changes

--- test.ml
+++ test.ml
@@ -3,6 +3,7 @@ open State
 open Actors
 open Js
 open Js_of_ocaml
+open Constants
 module Html = Dom_html
 
 let state1 = {init_st with
@@ -10,6 +11,63 @@ let state1 = {init_st with
   }
 
 let state2 = update_st state1
+
+
+let p = Regular {
+    move_speed = 0;
+    fly_speed = 0;
+    throw_power = 0;
+    recovery_time = 5;
+    direction = 0;
+    coordinate = (0, 0);
+    has_pillow = false;
+    img_src = "./pics/sprite_og.png";
+    who_threw = "na";
+    score = 0;
+    is_disabled = false;
+    last_time_disabled = 0.;
+  }
+
+let s = {
+  bloom = init_bloom;
+  soap = init_soap;
+  mcup = init_mcup;
+  pillows = [p];
+  collisions = [];
+  time = 0.;
+  last_time_of_pillow_spawn = 0.;
+  random_time = 0.;
+  game_start = Unix.gettimeofday ();
+  game_over = false;
+}
+
+
+let s' = {
+  bloom = init_bloom;
+  soap = init_soap;
+  mcup = Margarinecup {
+      move_speed = 4;
+      fly_speed = 8;
+      throw_power = 1;
+      recovery_time = 3;
+      direction = 1;
+      coordinate = (0, 0);
+      has_pillow = false;
+      img_src = "./pics/bloom.png";
+      who_threw = "na";
+      score = 0;
+      is_disabled = false;
+      last_time_disabled = 0.;
+    };
+  pillows = [];
+  collisions = [];
+  time = 0.;
+  last_time_of_pillow_spawn = 0.;
+  random_time = 0.;
+  game_start = Unix.gettimeofday ();
+  game_over = false;
+}
+
 
 let canvas = Opt.get(
     Opt.bind(Dom_html.document##getElementById(string "canvas"))
@@ -22,6 +80,10 @@ let state_tests = [
   "mcup_same" >:: (fun _ -> assert_equal state1.mcup state2.mcup);
   "draw_st" >:: (fun _ -> assert_equal state1
                     (Display.draw_state context state1; state1));
+  "gp1" >:: (fun _ -> assert_equal s'.bloom
+                (update_st s).bloom);
+  "gp2" >:: (fun _ -> assert_equal s'.mcup
+                (update_st s).mcup);
 ]
 
 let tests =
